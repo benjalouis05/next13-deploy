6460a6d44ddec33dd18f372d6b644687125330e8
benjalouis05 <91569415+benjalouis05@users.noreply.github.com>
2023-07-01 23:10:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        her: "guys",
+        welcome: "back",
+        to: "another",
+        video: ",",
+        please: "subscribe!",
+    });
+});
+
+app.listen(6969);
